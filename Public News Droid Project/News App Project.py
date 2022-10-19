1a06c81d86a37876fbe24df19c243ef50ed78a8c
Yakkeshwar <94734055+Yakkeshwar@users.noreply.github.com>
2022-10-19 11:13:50 +0530

    Update News App Project.py

--- Public News Droid Project/News App Project.py
+++ Public News Droid Project/News App Project.py
@@ -7,20 +7,18 @@ def getNews():
     url = "https://newsapi.org/v2/top-headlines?country=in&apiKey="+api_key
     news = requests.get(url).json()
 
-    articles = news["articles"]
+    articles = news["Articles"]
 
     my_articles = []
     my_news = ""
 
     for article in articles:
-        my_articles.append(article["title"])
+        my_articles.append(article["Title"])
 
     for i in range(10):
         my_news = my_news + str(i+1) + ". " + my_articles[i] + "\n" + "\n"
 
     label.config(text = my_news)
-
-
 
 canvas = tk.Tk()
 canvas.geometry("600x900")
